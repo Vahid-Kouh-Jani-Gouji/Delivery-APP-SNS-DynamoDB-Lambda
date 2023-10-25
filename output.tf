4181fb0e5b1d62955858f0f57378e7c997f9a18a
vahid koohjani <vk1604@yahoo.com>
2023-10-25 15:26:58 +0200

    init

---
+++ output.tf
@@ -0,0 +1,19 @@
+output "deliver_table_name" {
+  description = "Name of the environment DynamoDB table"
+  value       = aws_dynamodb_table.delivery.name
+}
+
+output "deliver_table_arn" {
+  description = "ARN of the environment DynamoDB table"
+  value       = aws_dynamodb_table.delivery.arn
+}
+
+output "base_url" {
+  description = "Base URL for API Gateway stage."
+
+  value = aws_apigatewayv2_stage.lambda.invoke_url
+}
+
+output "sqs_url" {
+  value = "${aws_sqs_queue.some_queue.id}"
+}
